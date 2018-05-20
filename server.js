6f5601835d1d00743117bb68ed0664ac20337d32
Saikat <saikats_5@yahoo.co.in>
2018-05-20 11:06:45 +0530

    file load using fs

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 var http = require('http');
+var fs = require('fs');
 
 function onRequest(request, response){
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+/*     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.write('Hello World');
-    response.end();
+    response.end(); */
+    fs.readFile('./index.html', null, function(error, data){
+        if(error){
+            response.writeHead(404);
+            response.write('File not found');
+        }else{
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(data);
+        }
+        response.end();
+    });
 }
 
 http.createServer(onRequest).listen(8000);
